e85fde930ac1afad06115fa0d2370f5be20bf980
Aman Sheikh <amansheikh3110@gmail.com>
2026-09-05 14:47:19 +0530

    Fix TypeScript typing for createOnLogin in PrivyProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -10,7 +10,6 @@ export default function Providers({ children }: { children: React.ReactNode }) {
       config={{
         // Create embedded wallets for users who don't have a wallet
         embeddedWallets: {
-          createOnLogin: 'users-without-wallets',
           ethereum: {
             createOnLogin: 'users-without-wallets'
           }
